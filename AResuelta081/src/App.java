157eba0652aeee51d171b05e23022f7a288208ed
AlbaPalacios12 <126180027+AlbaPalacios12@users.noreply.github.com>
2023-03-29 20:17:44 +0200

    Update App.java

--- AResuelta081/src/App.java
+++ AResuelta081/src/App.java
@@ -1,5 +1,20 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Hora r= new Hora (11, 30);
+        System.out.println(r);
+        for (int i = 1; i <= 61; i++) { //incrementamos 61 minutos
+            r.inc();
+        }
+        System.out.println(r); 
+        System.out.println("Escriba una hora: ");
+        int hora= new Scanner(System.in).nextInt();
+        boolean cambio = r.setHora (hora); //cambiamos la hora
+        if (cambio) {
+            System.out.println(r);
+        } else{
+            System.out.println("La hora no se ha cambiado");
+        }
     }
 }
